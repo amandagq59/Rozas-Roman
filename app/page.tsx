7a5c4e48a4593519df6b5689f6f53e9c8e449bce
Amanda Gómez Quero <amanda1758@gmail.com>
2026-02-08 13:15:05 +0100

    arreglos

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
         <div className="hero-heading-block">
           <h1>
             Tu solución legal comienza con
-            <span>Rozas & Román</span>
+            <span> Rozas & Román</span>
           </h1>
           <p>
             Acompañamos a cada cliente con asesoría legal clara y efectiva,
